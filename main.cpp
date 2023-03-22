ed3f6593d930a8aa29899ccef4ba1366fc97a7f7
Novak <novaldix@gmail.com>
2023-03-22 16:14:15 +0300

    13_6_2 added assert validation

--- main.cpp
+++ main.cpp
@@ -6,12 +6,15 @@ using std::vector;
 
 int main() {
     vector<float> prices = { 2.5, 4.25, 3.0, 10.0 };
-    vector<int> list = { 1, 1, 0, 3 };
+    vector<int> list = { 1, 1, 0, 3, 8 };
     float sum = 0;
 
     for (int i : list) {
-        assert(i >= 0 && i < prices.size());
-        sum += prices[i];
+        bool isItemValid = i >= 0 && i < prices.size();
+        assert(isItemValid);
+        if (isItemValid) {
+            sum += prices[i];
+        }
     }
 
     std::cout << sum << std::endl;
